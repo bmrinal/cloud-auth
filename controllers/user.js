41f16bf3eb618485a9a206d90a06e5a48923185c
Mrinal Purohit <mrinal@mrinallabs.com>
2020-05-13 15:53:20 -0400

    code refactoring

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 const respond = require('../utils/respond'); //responder (formats and sends the appropriate response codes and responses to the client)
 const adapter = require('../adapter'); //data adapter
 const dbops = require('../dbops'); //database operations
+
 const getToken = require('../utils/token-generator'); //JWT token generator
 const {
   check,
